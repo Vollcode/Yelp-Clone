b0b4bc259b153ff90b2cd4f5dee1ed251c960fa8
Daniel Ortiz <danielortiz@Daniels-MacBook-Pro-8.local>
2016-05-24 15:06:47 +0100

    Crud

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,19 @@
 class RestaurantsController < ApplicationController
   def index
-    # @restaurants = Restaurant.all
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
   end
 
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -11,13 +11,23 @@ feature 'restaurants' do
 
   context 'restaurants have been added' do
     before do
-      Restaurants.create(name: "KFC")
+      Restaurant.create(name: "KFC")
     end
 
     scenario 'display restaurants' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
       expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content('KFC')
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
