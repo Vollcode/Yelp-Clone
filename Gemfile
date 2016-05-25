ff2faebe29521e3722f42bf9d0aca488b390b85e
Alex Robertson <lex1989@gmail.com>
2016-05-25 15:16:42 +0100

    Add devise and facebook login

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 gem 'haml'
 gem 'devise'
+gem 'omniauth-facebook'
+gem 'dotenv-rails'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -21,6 +21,13 @@ Rails.application.routes.draw do
   end
 
 root "restaurants#index"
+
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+# devise_scope :user do
+#   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+# end
+
 
   # Example resource route with options:
   #   resources :products do
