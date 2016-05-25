fc8cd31d555bd0e64a7cb12188e9f4fcce14cfbe
Alex Robertson <lex1989@gmail.com>
2016-05-25 11:28:51 +0100

    Added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -18,6 +19,8 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
   end
+
+root "restaurants#index"
 
   # Example resource route with options:
   #   resources :products do
